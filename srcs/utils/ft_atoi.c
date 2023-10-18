bf77b77dbc49d104c1a6897bbdce007fdbc43123
Antoine MASSY <anmassy@paul-f4Br6s8.clusters.42paris.fr>
2023-10-18 12:02:59 +0200

    Update

---
+++ srcs/utils/ft_atoi.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: anmassy <anmassy@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/18 11:56:22 by anmassy           #+#    #+#             */
+/*   Updated: 2023/10/18 11:57:19 by anmassy          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../include/minishell"
+
+int	ft_atoi(const char *nptr)
+{
+	int	sign;
+	int	nb;
+	int	i;
+
+	sign = 1;
+	nb = 0;
+	i = 0;
+	while ((nptr[i] >= 9 && nptr[i] <= 13) || nptr[i] == 32)
+		i++;
+	if (nptr[i] == '-' || nptr[i] == '+')
+	{
+		if (nptr[i] == '-')
+			sign = -sign;
+		i++;
+	}
+	while (nptr[i] >= '0' && nptr[i] <= '9')
+	{
+		nb = nb * 10 + (nptr[i] - 48);
+		i++;
+	}
+	return (sign * nb);
+}
---
+++ srcs/utils/ft_itoa..c
@@ -0,0 +1,59 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_itoa..c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: anmassy <anmassy@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/18 11:56:18 by anmassy           #+#    #+#             */
+/*   Updated: 2023/10/18 11:57:45 by anmassy          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../include/minishell"
+
+static size_t	ft_strlen_nbr(long nb)
+{
+	size_t	len;
+
+	len = 1;
+	if (nb < 0)
+	{
+		nb = nb * -1;
+		len++;
+	}
+	while (nb > 9)
+	{
+		nb = nb / 10;
+		len++;
+	}
+	return (len);
+}
+
+char	*ft_itoa(int n)
+{
+	long	nb;
+	int		len;
+	char	*string;
+
+	nb = n;
+	len = ft_strlen_nbr(nb);
+	string = (char *)malloc((len + 1) * sizeof(char));
+	if (!string)
+		return (NULL);
+	string[len--] = '\0';
+	if (nb == 0)
+		string[0] = '0';
+	if (nb < 0)
+	{
+		string[0] = '-';
+		nb = nb * -1;
+	}
+	while (nb > 0)
+	{
+		string[len] = (nb % 10) + 48;
+		nb = nb / 10;
+		len--;
+	}
+	return (string);
+}
